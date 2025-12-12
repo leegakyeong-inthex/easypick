34cc04adc17358fed0ddb89a4894d56c0f94f570
leegakyeong-inthex <leegakyeong@inthe-x.com>
2025-12-12 13:49:46 +0900

    move navigation bar to layout

--- app/page.js
+++ app/page.js
@@ -6,33 +6,33 @@ import Link from "next/link";
 import { Sheet } from "react-modal-sheet";
 import styles from "./page.module.css";
 
-const tabs = [
-  {
-    title: "검색",
-    iconUrl: "/images/icons/pageview.png",
-    href: "/",
-  },
-  {
-    title: "내 카드",
-    iconUrl: "/images/icons/add_card.png",
-    href: "/",
-  },
-  {
-    title: "혜택 지도",
-    iconUrl: "/images/icons/pin_drop.png",
-    href: "/",
-  },
-  {
-    title: "카드 추천",
-    iconUrl: "/images/icons/credit_card.png",
-    href: "/",
-  },
-  {
-    title: "더보기",
-    iconUrl: "/images/icons/menu.png",
-    href: "/",
-  },
-]
+// const tabs = [
+//   {
+//     title: "검색",
+//     iconUrl: "/images/icons/pageview.png",
+//     href: "/",
+//   },
+//   {
+//     title: "내 카드",
+//     iconUrl: "/images/icons/add_card.png",
+//     href: "/",
+//   },
+//   {
+//     title: "혜택 지도",
+//     iconUrl: "/images/icons/pin_drop.png",
+//     href: "/",
+//   },
+//   {
+//     title: "카드 추천",
+//     iconUrl: "/images/icons/credit_card.png",
+//     href: "/",
+//   },
+//   {
+//     title: "더보기",
+//     iconUrl: "/images/icons/menu.png",
+//     href: "/",
+//   },
+// ]
 
 const places = [
   {
@@ -123,7 +123,8 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="h-screen relative">
+    // <div className="h-screen relative">
+    <div>
       <div className="absolute top-0 w-full h-[60px] rounded-b-2xl flex justify-center" style={{ boxShadow: '0px 4px 10px 0px rgba(0, 0, 0, 0.08)' }}>
         <div className="w-fit h-fit flex items-center mt-1.5 rounded-full font-semibold text-base border border-[#EBEBEB] p-px bg-[#F2F2F2] leading-none">
           <div className="rounded-full bg-white px-3.5 py-[9px] text-[#070707]" style={{ boxShadow: '0px 0px 24px 0px rgba(0, 0, 0, 0.16)'}}>장소</div>
@@ -290,7 +291,7 @@ export default function Home() {
         {/* <Sheet.Backdrop style={{ zIndex: 500 }} /> */}
       </Sheet>
 
-      <div className="absolute z-50 bottom-0 w-full h-20 pt-2.5 px-[25px] bg-white" style={{ boxShadow: '0px 4px 10px 0px rgba(0, 0, 0, 0.08)' }}>
+      {/* <div className="absolute z-50 bottom-0 w-full h-20 pt-2.5 px-[25px] bg-white" style={{ boxShadow: '0px 4px 10px 0px rgba(0, 0, 0, 0.08)' }}>
         <div className="flex items-center space-x-[30px]">
           {tabs.map((tab) => (
             <Link key={tab.title} href={tab.href} className="w-[41px] h-[41px] flex flex-col items-center space-y-0.5">
@@ -299,7 +300,7 @@ export default function Home() {
             </Link>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
