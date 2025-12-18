b3836cbf6deb0660151d2b6d323ba97eb1fb3ac9
leegakyeong-inthex <leegakyeong@inthe-x.com>
2025-12-19 01:56:18 +0900

    style CardRegistration

--- components/pages/CardRegistration.tsx
+++ components/pages/CardRegistration.tsx
@@ -11,50 +11,62 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: true,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: true,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -64,14 +76,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -81,14 +96,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -98,14 +116,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -115,14 +136,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -132,14 +156,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -149,14 +176,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -166,14 +196,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -183,14 +216,17 @@ const cards = [
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 국민 CLiP 카드',
+        selected: false,
       },
       {
         image: '/images/card-2.png',
         name: 'KB 청춘대로 톡톡카드',
+        selected: false,
       },
     ],
   },
@@ -203,7 +239,7 @@ export default function CardRegistration({ setIsVisible }) {
 
   return (
     <>
-      <div style={{ zIndex: 2 }} className="bg-white absolute w-full h-full">
+      <div style={{ zIndex: 2 }} className="bg-white absolute w-screen h-screen flex flex-col">
         <div className="flex items-center pt-3.5 pb-2.5 px-[17px] mb-[18px]">
           <Image
             src="/images/icons/arrow-left.png"
@@ -215,13 +251,13 @@ export default function CardRegistration({ setIsVisible }) {
           />
           <div className="text-xl font-bold">내 카드 등록</div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex-1 min-h-0 flex flex-col bg-white">
           <div className="flex items-center border-b border-[#F4F4F4] font-semibold text-xs text-[#6D727A]">
             <div className="w-[134px] pl-[18px] pb-3.5">카드사</div>
             <div className="pl-[18px] pb-3.5">카드</div>
           </div>
-          <div className="flex flex-1">
-            <div className="border-r border-[#F4F4F4] w-[134px] pt-1.5 pr-1.5">
+          <div className="flex flex-1 min-h-0">
+            <div className="border-r border-[#F4F4F4] w-[134px] pt-1.5 pr-1.5 overflow-y-scroll">
               {cards.map((card) => (
                 <div
                   key={card.company}
@@ -230,22 +266,25 @@ export default function CardRegistration({ setIsVisible }) {
                 >{card.company}</div>
               ))}
             </div>
-            <div className="flex flex-col py-1.5 px-2.5 space-y-1.5 flex-1 overflow-y-scroll h-full">
+            <div className="flex flex-col py-1.5 px-2.5 space-y-1.5 flex-1 overflow-y-scroll">
               {cards.find((card) => card.company === selectedCard).cards.map((card,i) => (
                 <div
                   key={card.name+i}
-                  className="flex items-center w-full h-[60px] font-medium text-sm"
+                  className={`flex items-center w-full min-h-[60px] font-medium text-sm rounded-[10px] px-[13px] ${card.selected ? 'bg-[#0B0D0F] text-white' : ''}`}
                   onClick={() => setIsRegistrationOpen(true)}
                 >
-                  <Image src={card.image} width={30} height={44} alt="" className="mr-1.5" />
+                  <Image src={card.image} width={26} height={40} alt="" className="mr-1.5" />
                   <div>{card.name}</div>
                 </div>
               ))}
             </div>
           </div>
-          <div>
+          <div className="pt-2.5 px-[18px] h-[130px] flex flex-col items-center" style={{ boxShadow: '0px -4px 10px 0px rgba(0, 0, 0, 0.04)' }}>
             <Button>확인</Button>
-            <div className="underline" onClick={() => setIsOpen(true)}>찾는 카드가 없나요?</div>
+            <div
+              className="underline mt-[19px] font-semibold text-sm text-[#6D727A]"
+              onClick={() => setIsOpen(true)}
+            >찾는 카드가 없나요?</div>
           </div>
         </div>
       </div>
@@ -275,19 +314,22 @@ export default function CardRegistration({ setIsVisible }) {
       <Sheet
         isOpen={isRegistrationOpen}
         onClose={() => setIsOpen(false)}
-        snapPoints={[0, 200, 1]}
+        snapPoints={[0, 420, 1]}
         // detent="content"
         initialSnap={1}
       >
-        <Sheet.Header />
         <Sheet.Container>
+          <Sheet.Header />
           <Sheet.Content>
-            <div>
-              <div>삼성 카드</div>
-              <div>모니모 카드</div>
-              <Image src="/images/card-3.png" width={140} height={87} alt="" className="rotate-90" />
-              <Input placeholder="00 / 00" />
-              <Button>확인</Button>
+            <div className="flex flex-col items-center mt-1.5 px-[18px]">
+              <div className="font-semibold text-[13px] text-[#0068FF] mb-1">삼성카드</div>
+              <div className="font-semibold text-xl mb-5">모니모 카드</div>
+              <Image src="/images/card-3.png" width={140} height={87} alt="" className="rotate-90 -mt-12 -mb-5" />
+              <div className="flex items-center w-full">
+                <label className="font-semibold text-sm text-[#686B70] ml-[18px] absolute">유효기간</label>
+                <Input className="bg-[#F3F3F3] text-right w-full h-[62px] mb-6" placeholder="00 / 00" />
+              </div>
+              <Button className="mb-6">완료</Button>
             </div>
           </Sheet.Content>
         </Sheet.Container>
